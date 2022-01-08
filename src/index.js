a0977987693fb34775edc82f5818519324110716
spell-landon <spell.landon@gmail.com>
2022-01-08 16:56:15 -0600

    Add index to index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <App index />
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
